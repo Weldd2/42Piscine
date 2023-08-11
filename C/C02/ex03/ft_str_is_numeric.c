8d888dbbc79f0a1d5679741e2712fb36c473a183
Antoine Mura <amura@c1r7p7.42lehavre.fr>
2023-08-11 11:40:50 +0200

    C02 ex03

--- C/C02/ex03/ft_str_is_numeric.c
+++ C/C02/ex03/ft_str_is_numeric.c
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amura <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/11 10:46:18 by amura             #+#    #+#             */
-/*   Updated: 2023/08/11 11:30:15 by amura            ###   ########.fr       */
+/*   Created: 2023/08/11 11:32:15 by amura             #+#    #+#             */
+/*   Updated: 2023/08/11 11:40:06 by amura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_alpha(char *str)
+int	ft_str_is_numeric(char *str)
 {
 	int	compteur;
 
 	compteur = 0;
 	while (str[compteur])
 	{
-		if ((str[compteur] < 'A' || str[compteur] > 'Z') 
-			&& (str[compteur] < 'a' || str[compteur] > 'z'))
+		if (str[compteur] > '9' || str[compteur] < '0')
 		{
 			return (0);
 		}
