a327ca6bef9c695aa8d90a91293abd462015bd7e
Antoine Mura <amura@c1r7p7.42lehavre.fr>
2023-08-11 10:30:47 +0200

    C02 ex00

---
+++ C/C02/ex00/ft_strcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amura <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/11 10:12:03 by amura             #+#    #+#             */
+/*   Updated: 2023/08/11 10:25:00 by amura            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	compteur;
+
+	compteur = 0;
+	while(src[compteur])
+	{
+		dest[compteur] = src[compteur];
+		compteur++;
+	}
+	dest[compteur] = '\0';
+	return *dest;
+}
