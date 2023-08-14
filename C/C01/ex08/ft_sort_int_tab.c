375fcf2282d8218b0895eb457f3b328796757b79
Antoine Mura <amura@c1r7p7.42lehavre.fr>
2023-08-14 17:17:59 +0200

    correction exo 8

--- C/C01/ex08/ft_sort_int_tab.c
+++ C/C01/ex08/ft_sort_int_tab.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amura <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: amura <amura@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 09:58:25 by amura             #+#    #+#             */
-/*   Updated: 2023/08/11 10:09:05 by amura            ###   ########.fr       */
+/*   Updated: 2023/08/14 17:17:03 by amura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,14 @@ void	ft_sort_int_tab(int *tab, int size)
 
 	compteur = 0;
 	temp = -1;
-	while (compteur < (size - 1))
+	while (compteur < (size - 1) && (tab[compteur]))
 	{
 		while (tab[compteur] > tab[compteur + 1])
 		{
 			temp = tab[compteur];
 			tab[compteur] = tab[compteur + 1];
 			tab[compteur + 1] = temp;
-			if (compteur > 0)
-			{
-				compteur--;
-			}
+			compteur = 0;
 		}
 		compteur++;
 	}
