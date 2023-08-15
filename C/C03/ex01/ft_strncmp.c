3a89576c3d60b7d0f0c92b2e3b001ceaf6ccdd0b
antoine <a.mura27400@gmail.com>
2023-08-15 13:50:32 +0200

    C03 ex00 ex01

---
+++ C/C03/ex01/ft_strncmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amura <amura@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 13:38:20 by amura             #+#    #+#             */
+/*   Updated: 2023/08/15 13:48:40 by amura            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while ((s1[i] && s2[i]) || i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	int i = ft_strncmp("abc", "abc", 5);
+
+	printf("%d", i);
+}
