2c91f3f4d97f39a97b53f8a9df4cdd63ad1f1689
antoine <a.mura27400@gmail.com>
2023-08-17 23:01:13 +0200

    C07 ex03

--- C/C07/ex03/ft_strjoin.c
+++ C/C07/ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: amura <amura@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 20:10:12 by amura             #+#    #+#             */
-/*   Updated: 2023/08/17 22:56:06 by amura            ###   ########.fr       */
+/*   Updated: 2023/08/17 23:00:52 by amura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 int	ft_strlen(char *str)
 {
-    int i = 0;
+	int	i;
 
-    while (str[i])
-        i++;
-    return (i);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-char	*concat_str(char *src, char* dest)
+char	*concat_str(char *src, char *dest)
 {
 	int	i;
 	int	dest_length;
-	
+
 	dest_length = ft_strlen(dest);
 	i = 0;
 	while (src[i] != '\0')
@@ -41,7 +42,7 @@ int	calcul_full_length(int size, char **strs, char *sep)
 {
 	int	i;
 	int	total_length;
-	
+
 	i = 0;
 	total_length = 0;
 	while (i < size)
@@ -53,18 +54,18 @@ int	calcul_full_length(int size, char **strs, char *sep)
 	return (total_length);
 }
 
-char *ft_strjoin(int size, char **strs, char *sep)
+char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	int	i;
+	int		i;
+	int		full_length;
 	char	*str;
-	int	full_length;
 
 	i = 0;
 	if (!size)
 		return (malloc(0));
 	full_length = calcul_full_length(size, strs, sep);
 	str = malloc(full_length + 1);
-	while(i < size)
+	while (i < size)
 	{
 		concat_str(strs[i], str);
 		if (i != size + 1)
