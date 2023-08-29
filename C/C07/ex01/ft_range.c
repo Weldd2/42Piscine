4768c25da9c0eb9257a8c1571a3cc601101a8df6
antoine <a.mura27400@gmail.com>
2023-08-29 20:56:13 +0200

    C07 fin

--- C/C07/ex01/ft_range.c
+++ C/C07/ex01/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: amura <amura@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 19:43:24 by amura             #+#    #+#             */
-/*   Updated: 2023/08/29 15:00:00 by amura            ###   ########.fr       */
+/*   Updated: 2023/08/29 20:49:53 by amura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,4 @@ int	*ft_range(int min, int max)
 	while (min < max)
 		tab[++i] = min++;
 	return (tab);
-}
-
-#include <stdio.h>
-int	main(void)
-{
-	int	*s = ft_range(5, 4);
-	for (int i = 0; i < 5; i++)
-	{
-		printf("%d\n", s[i]);
-		
-	}
 }
--- C/C07/ex05/ft_split.c
+++ C/C07/ex05/ft_split.c
@@ -6,16 +6,16 @@
 /*   By: amura <amura@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 12:28:11 by amura             #+#    #+#             */
-/*   Updated: 2023/08/29 16:56:29 by amura            ###   ########.fr       */
+/*   Updated: 2023/08/29 20:51:19 by amura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int	char_in_charset(char c, char *charset)
 {
-	while(*charset)
+	while (*charset)
 	{
 		if (c == *charset)
 			return (1);
@@ -23,92 +23,69 @@ int	char_in_charset(char c, char *charset)
 	}
 	return (0);
 }
+
 int	count_words(char *str, char *charset)
 {
 	int	i;
 
 	i = 0;
-	while (*str != '\0')
+	while (*str)
 	{
-		if(char_in_charset(*str, charset))
+		if (char_in_charset(*str, charset))
 			i++;
 		str++;
 	}
 	return (i + 1);
 }
 
+void	ft_fill_word(char **r, char **str, char *charset)
+{
+	char	*r_start;
+
+	r_start = *r;
+	while (**str && !char_in_charset(**str, charset))
+	{
+		**r = **str;
+		(*str)++;
+		(*r)++;
+	}
+	**r = '\0';
+	*r = r_start;
+	while (**str && char_in_charset(**str, charset))
+		(*str)++;
+}
+
+int	ft_wordlen(char *str, char *charset)
+{
+	int	len;
+
+	len = 0;
+	while (*str && !char_in_charset(*str, charset))
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
 char	**ft_split(char *str, char *charset)
 {
 	char	**r;
 	char	**r_cpy;
-	int		str_len;
 
-	r = malloc(count_words(str, charset) * sizeof(char *));
+	r = malloc((count_words(str, charset) + 1) * sizeof(char *));
+	if (!r)
+		return (NULL);
 	r_cpy = r;
-	while (*str != '\0')
+	while (*str)
 	{
-		str_len = -1;
-		while (str[++str_len])
-			;
-		*r = malloc(str_len);
-		while (!char_in_charset(*str, charset))
-		{
-			**r = *str;
-			str++;
-			(*r)++;
-		}
-		**r = '\0';
-		str++;
-		r++;
+		*r = malloc(ft_wordlen(str, charset) + 1);
+		if (!*r)
+			return (NULL);
+		ft_fill_word(r, &str, charset);
+		if (*str)
+			r++;
 	}
-	*r = NULL;
+	*(r + 1) = NULL;
 	return (r_cpy);
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-}
-
-void	print_split_strings(char **tab)
-{
-	int	i;
-
-	i = 0;
-	while (tab[i])
-	{
-		ft_putstr(tab[i]);
-		ft_putchar('\n');
-		free(tab[i]);
-		i++;
-	}
-	free(tab);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	// char	**result;
-	char	*s;
-
-	s = "hello world this is split";
-	ft_putstr("---- Test 1 ----\n");
-	printf("%d\n", count_words(s, " "));
-	// result = ft_split(s, " ");
-	// print_split_strings(result);
-
-	return (0);
 }
